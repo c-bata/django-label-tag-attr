80d8cd468784a62e2d01612206996a31844b85de
c-bata <contact@c-bata.link>
2015-03-29 12:27:22 +0900

    Add test

--- label_tag_attr/tests/test_label_tag_attr.py
+++ label_tag_attr/tests/test_label_tag_attr.py
@@ -1,6 +1,25 @@
+from django import forms
+from django.template import Template, Context
 from django.test import TestCase
 
 
-class TestFieldAttributeNode(TestCase):
-    pass
+LIBRARY_LOAD_STRING = '{% load label_tag_attr %}'
 
+
+class MyForm(forms.Form):
+    sample_field = forms.CharField(label='SampleText')
+
+
+def render_form(text, form=None, **context_args):
+    tpl = Template(LIBRARY_LOAD_STRING + text)
+    context_args.update({'form': MyForm() if form is None else form})
+    context = Context(context_args)
+    return tpl.render(context)
+
+
+class TestFieldAttributeNode(TestCase):
+    def test_rendered_label_has_id(self):
+        res = render_form('{% render_label form.sample_field %}',
+                          form=MyForm())
+        self.assertIn('for="id_sample_field"', res)
+        self.assertIn('SampleText', res)
