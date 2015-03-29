1ed07ca1446031c5391275046c0be47286598ed3
c-bata <contact@c-bata.link>
2015-03-29 15:25:48 +0900

    Remove files

--- example/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from example.models import SampleModel
-
-
-class SampleModelForm(forms.ModelForm):
-    """sample model's form"""
-    class Meta:
-        model = SampleModel
-        fields = ('text', 'choices',)
--- example/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from example.forms import SampleModelForm
-
-
-def home(request):
-    form = SampleModelForm()
-    return render_to_response('example/home.html', {'request': request,
-                                                    'form': form})
